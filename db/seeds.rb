7f2b4abc393076aa7b241911eb388bdf1daaa0cd
akash chhetri <akash.chhetri1@gmail.com>
2021-04-24 12:26:02 +1000

    flash message added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 5.times {
   user = User.create(username: "akash#{rand(100)}", password: "password")
-  Message.create(body: "Hi I am a new message from #{user.username}", user: user)
+  Message.create(body: "Hi I am a new message from #{user.username}", user_id: user.id) unless user.id.nil?
 }
